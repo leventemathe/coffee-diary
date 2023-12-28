20f7d054cb12acf10b00245e634f8c2a57f4d7b4
Levente Máthé <levente.l.mathe@gmail.com>
2023-12-28 21:04:22 +0100

    add more constraints

--- backend/models/brew.go
+++ backend/models/brew.go
@@ -7,15 +7,15 @@ import (
 type Brew struct {
 	gorm.Model
 	CoffeeID      uint        `gorm:"not null;default:null;index"`
-	Coffee        Coffee      `gorm:"not null;default:null"`
+	Coffee        Coffee      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	CoffeeMakerID uint        `gorm:"not null;default:null;index"`
-	CoffeeMaker   CoffeeMaker `gorm:"not null;default:null"`
+	CoffeeMaker   CoffeeMaker `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	GrinderID     uint        `gorm:"not null;default:null;index"`
-	Grinder       Grinder     `gorm:"not null;default:null"`
+	Grinder       Grinder     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	Input         uint        `gorm:"not null;default:null"`
 	Output        uint        `gorm:"not null;default:null"`
 	Time          uint        `gorm:"not null;default:null"`
-	GrindSetting  float32
+	GrindSetting  float32     `gorm:"not null;default:null"`
 	Temperature   *float32
 	Pressure      *float32
 	Preinfustion  *bool
